3087468d51aaa1d0a7f430ae6361b91faf0c7aae
Piyush <piyush.bijwal@gmail.com>
2018-10-07 22:28:16 +1100

    Postgres database insert included

--- get_mf_history.py
+++ get_mf_history.py
@@ -1,4 +1,3 @@
-
 #############################################
 # Author: Piyush Bijwal
 #############################################
@@ -9,72 +8,89 @@ import datetime
 import time
 import glob
 import pandas as pd
+from sqlalchemy import create_engine as ce
+import os
 
 def echo_msg(t_str):
 		print(datetime.datetime.now().strftime("%Y%m%d %H%M%S")+'::: '+t_str)
 
-def fun_url_process(i_mf_cnt,i_tp_cnt,start_dt,end_dt):
+def fun_url_process(i_mf_cnt,i_tp_cnt,start_dt,end_dt,a_filealias,p_filepath):
 	url='http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?mf='+str(i_mf_cnt)+'&tp='+str(i_tp_cnt)+'&frmdt='+start_dt+'&todt='+end_dt
 	echo_msg('Inside the loop'+str(i_mf_cnt)+str(i_tp_cnt))
 	echo_msg('URL:::'+url)
 	r = requests.get(url, allow_redirects=True)
 	echo_msg(datetime.datetime.now().strftime("%Y%m%d %H%M%S")+'::: After request call')
-	f_filename=str(i_mf_cnt)+'_'+str(i_tp_cnt)+'.txt'
+	f_filename=p_filepath+str(i_mf_cnt)+'_'+str(i_tp_cnt)+a_filealias
 	if (r.content.find('Scheme Code') == 0):
 		echo_msg('Inside if statement')
 		open(f_filename, 'wb').write(r.content)
 
+def fun_conndb(df_4db,i_tab_nme, i_action):
+	dbengine = ce('postgresql://piyushbijwal:"9!En087654321"@localhost:5432/piyushbijwal')
+	if i_action == 'I':
+		df_4db.head(0).to_sql(i_tab_nme, con=dbengine,if_exists='append')
+		df_4db.to_sql(i_tab_nme, con=dbengine,if_exists='append')
+
 def fun_process_file(f_file):
-	#f_filename='9_1.txt'
+	#f_file='9_1.txt'
 	f=open(f_file,'r')
 	#f.read()
 	t_title_list=[]
 #	echo_msg('File successfully opened')
-	title_list=f.readline().split(';')
-	title_list = [x.replace(' ','') for x in title_list]
-	fin_list=[]
+	l_title_list=f.readline().split(';')
+	l_title_list = [x.replace(' ','') for x in l_title_list]
+	l_fin_list=[]
 	for line in f.read().splitlines():
 		t_list= line.split(';')
-		fin_list.append(t_list)
+		l_fin_list.append(t_list)
 #	echo_msg('End of for loop')
-	df = pd.DataFrame(fin_list,columns=title_list)
+	df = pd.DataFrame(l_fin_list,columns=l_title_list)
 #Start: Logic to extract fund house name
+	df = df.loc[df.SchemeCode.str.isdigit()]
 	t_lst=pd.DataFrame(df['SchemeCode'].unique().tolist())
 	t_mf_house=t_lst[t_lst[0].str.contains('Mutual Fund')].iloc[0][0]
 	df['FundHouse']=t_mf_house
+	fun_conndb(df,'t_mf_txn','I')
 	echo_msg(f_file+'-'+t_mf_house)
 #End: Logic to extract fund house name
 #Start: Logic to map Scheme type and scheme code
-# Capture the derived data in dataframe and load it into seperate table/file
 	t_scheme_name=''
+	l_mf_schtyp=[]
+	l_title_list=['FundHouse','SchemeType','SchemeCode']
+	df_mf_sch_typ = pd.DataFrame(columns=l_title_list)
 	for index, row in t_lst.iterrows():
 		t_val=row.iloc[0]
 		if 'Mutual Fund' not in t_val:
 			if t_val.isdigit():
-				t_str = t_scheme_name+';'+str(t_val)
-				print t_str
+				t_list=[t_mf_house,t_scheme_name,str(t_val)]
+				l_mf_schtyp.append(t_list)
+#				t_str=t_mf_house+";"+t_scheme_name+";"+str(t_val)
+#				print l_mf_schtyp
 			else:
 				t_scheme_name=t_val
+	df_mf_sch_typ = pd.DataFrame(l_mf_schtyp,columns=l_title_list)
+	fun_conndb(df_mf_sch_typ,'t_mf_sch_type','I')
+#	print df_mf_sch_typ
+#End: Logic to map Scheme type and scheme code
 
-
-#	df = df[~df['Scheme Code'].isnull()] 
 
 start_dt = (datetime.datetime.now() - datetime.timedelta(days=365.2425 * 10)).strftime('%d-%b-%Y')
 end_dt = datetime.datetime.now().strftime("%d-%b-%Y")
-
+a_filealias = "_"+datetime.datetime.now().strftime("%Y%m%d")+'mflst.txt'
+p_filepath='/Users/piyushbijwal/Documents/Project/stock_analysis/MyScripts/DataSet/'
 
 i_mf_cnt=1
-while (i_mf_cnt!=100):
+while (i_mf_cnt!=2):
 	i_mf_cnt=i_mf_cnt+1
 #	echo_msg('Calling Open Fund Function'+str(i_mf_cnt))
 ##Open fund
-	fun_url_process(i_mf_cnt,1,start_dt,end_dt)
+	fun_url_process(i_mf_cnt,1,start_dt,end_dt,a_filealias,p_filepath)
 	time.sleep(20)
 
 ###### Below code in progress. It would help to process the file
-#os.chdir("/mydir")
-for f_filename in glob.glob("9_*.txt"):
-#    echo_msg(f_filename)
+os.chdir(p_filepath)
+for f_filename in glob.glob('*'+a_filealias):
+    echo_msg(f_filename)
     fun_process_file(f_filename)
 
 
